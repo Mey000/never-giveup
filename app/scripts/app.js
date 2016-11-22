b6e78a99af2b900bef680c454635d12ba8946e8c
Mey000 <1421005308@qq.com>
2016-11-22 10:47:16 +0800

    初始化项目

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name quexianguanliApp
+ * @description
+ * # quexianguanliApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('quexianguanliApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quexianguanliApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quexianguanliApp
+ */
+angular.module('quexianguanliApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
